172ee8971dc17b27f57a763825b9ca0e3479242b
Muhammad Haiqal <muhammadhaiqal1802@gmail.com>
2023-09-03 06:44:05 +0700

    handle delete

--- src/pages/Images.jsx
+++ src/pages/Images.jsx
@@ -43,6 +43,20 @@ function Images() {
 		}
 	}
 
+	const handleDelete = async (imageId) => {
+		try {
+			const isConfirmed = window.confirm('Are you sure you want to delete?')
+
+			if (!isConfirmed) {
+				return
+			}
+			await axios.delete(`http://localhost:5000/images/${imageId}`)
+			fetchAllImages()
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	useEffect(() => {
 		fetchAllImages()
 	}, [])
@@ -130,6 +144,29 @@ function Images() {
 											imageId={image.id}
 											initialImage={image}
 										/>
+										<button
+											onClick={() => handleDelete(image.id)}
+											className='text-white'
+										>
+											<svg
+												xmlns='http://www.w3.org/2000/svg'
+												className='w-5 h-5 text-white hover:scale-110'
+												viewBox='0 0 24 24'
+												strokeWidth='1.5'
+												stroke='currentColor'
+												fill='none'
+												strokeLinecap='round'
+												strokeLinejoin='round'
+											>
+												<path
+													stroke='none'
+													d='M0 0h24v24H0z'
+													fill='none'
+												></path>
+												<line x1='18' y1='6' x2='6' y2='18'></line>
+												<line x1='6' y1='6' x2='18' y2='18'></line>
+											</svg>
+										</button>
 									</div>
 								</div>
 								<button
